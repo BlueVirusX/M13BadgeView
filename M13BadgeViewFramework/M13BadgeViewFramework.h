ed286558557dcffbbb10b855767a2db00ce49b9f
BlueVirusX <patrick.ganaus@outlook.com>
2016-06-13 15:08:03 +0200

    Added carthage support
    Added carthage support
    Added framework target
    Fixed warnings

---
+++ M13BadgeViewFramework/M13BadgeViewFramework.h
@@ -0,0 +1,18 @@
+//
+//  M13BadgeViewFramework.h
+//  M13BadgeViewFramework
+//
+//  Created by Ganaus, Patrick on 13/06/16.
+//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M13BadgeViewFramework.
+FOUNDATION_EXPORT double M13BadgeViewFrameworkVersionNumber;
+
+//! Project version string for M13BadgeViewFramework.
+FOUNDATION_EXPORT const unsigned char M13BadgeViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M13BadgeViewFramework/PublicHeader.h>
+#import <M13BadgeView/M13BadgeView.h>
